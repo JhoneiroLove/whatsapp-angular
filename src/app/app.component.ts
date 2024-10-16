d38f9f5a533c9f3ad7c17368709fb62fa8cf013e
jhoneiro <jhoneiro12@hotmail.com>
2024-10-16 01:15:39 -0500

    app de prueba de concepto para integracion de whatsapp con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true, // definir como standalone
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'whatsapp-integration';
+  phoneNumber: string = '';
+  messageText: string = '';
+
+  // Método para enviar el mensaje
+  sendMessage() {
+    const phone = this.phoneNumber.startsWith('+') ? this.phoneNumber : `+${this.phoneNumber}`;
+    const encodedMessage = encodeURIComponent(this.messageText);
+    const url = `https://web.whatsapp.com/send?phone=${phone}&text=${encodedMessage}`; // Para abrir en la web de WhatsApp
+    //const url = `https://wa.me/${phone}?text=${encodedMessage}`; -> Para abrir en la app de WhatsApp
+    window.open(url, '_blank');
+  }
 }
